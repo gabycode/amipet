ea988874aa0510a85a7cc23e1916cfc805a55f08
Gabriella Rosario <g.rosario@gruporamos.com>
2025-07-02 08:14:13 -0400

    add: font and weight

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,10 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'Agrandir',
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(fontWeight: FontWeight.w600),
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
